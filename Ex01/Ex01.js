142f266ee4c90238c63351a8f36ca7a8f3ba2ea4
Hanikinguyen <95458424+Hanikinguyen@users.noreply.github.com>
2026-06-02 13:51:55 +0700

    Upload bài tập 01 của SS14

---
+++ Ex01/Ex01.js
@@ -0,0 +1,11 @@
+const person = {
+  name: "John Doe",
+  age: 25,
+  job: "Developer"
+};
+
+// In ra giá trị thuộc tính name
+console.log(person.name);
+
+// In ra toàn bộ đối tượng
+console.log(person);
