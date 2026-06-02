21f27471b06fbb9be0fe3d00aa9a75b5ca787633
Hanikinguyen <95458424+Hanikinguyen@users.noreply.github.com>
2026-06-02 13:59:09 +0700

    Upload bài tập 03 của SS14

---
+++ Ex03/Ex03.js
@@ -0,0 +1,11 @@
+const book = {
+  title: "JavaScript Basics",
+  author: "John Smith",
+  page: 200
+};
+
+// Xóa thuộc tính page
+delete book.page;
+
+// In ra đối tượng sau khi xóa
+console.log(book);
